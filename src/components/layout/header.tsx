989863a0ebdcf53b7f281696c267d5d8826855a8
Fattybun <fattybun1128@gmail.com>
2024-11-07 23:17:46 +0800

    implement header ui

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { route } from "@/configs/route";
+import { MoonStar, Sun } from "lucide-react";
+
+const Header: React.FC = () => {
+  const theme = true ? <MoonStar className="cursor-pointer" /> : <Sun />;
+  const routes = [
+    {
+      label: route.home,
+      url: "/",
+    },
+    {
+      label: route.blog,
+      url: "/blog",
+    },
+    {
+      label: route.collaborate,
+      url: "/collaborate",
+    },
+    {
+      label: route.music,
+      url: "/music",
+    },
+    {
+      label: route.about,
+      url: "/about",
+    },
+  ];
+
+  return (
+    <header className="w-full py-5 px-40 bg-white shadow-md fixed top-0">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="text-2xl font-bold text-blue-600">Bunventures</div>
+
+        <div className="flex gap-3 items-center">
+          <nav className="flex gap-3">
+            {routes.map((route, index) => (
+              <Button variant="link" className="text-black" key={index} asChild>
+                <Link href={route.url}>{route.label}</Link>
+              </Button>
+            ))}
+          </nav>
+
+          {theme}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/configs/route.ts
@@ -0,0 +1,7 @@
+export const route = {
+  home: "Home",
+  blog: "Blog",
+  collaborate: "Collaborate",
+  music: "Music",
+  about: "About",
+} as const;
