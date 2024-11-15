729d215953f49f54982e833af8ff896193f2df47
Fattybun <fattybun1128@gmail.com>
2024-11-16 01:59:43 +0800

    Update About Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,16 @@
+import Resume from "@/components/layout/resume";
 import Landing from "@/components/layout/landing";
 
 const About = () => {
   return (
-    <Landing
-      backgroundImage="/about.png"
-      heading="A Journey in Design and Innovation"
-      description="Welcome to my story! I'm a digital creator passionate about merging creativity with functionality. With a dedication to user-centered design and innovative problem-solving, I strive to make a positive impact through my work. Discover more about my background, values, and the journey that fuels my vision in the digital world."
-    />
+    <>
+      <Landing
+        backgroundImage="/about.png"
+        heading="A Journey in Design and Innovation"
+        description="Welcome to my story! I'm a digital creator passionate about merging creativity with functionality. With a dedication to user-centered design and innovative problem-solving, I strive to make a positive impact through my work. Discover more about my background, values, and the journey that fuels my vision in the digital world."
+      />
+      <Resume />
+    </>
   );
 };
 
--- src/configs/resume.ts
+++ src/configs/resume.ts
@@ -23,16 +23,44 @@ export const frameworks = {
   ionic: "Ionic",
   nextjs: "Next.js",
   react: "React",
-  shadcnui: "shadcn/ui",
-  styledcomponents: "styled-components",
-  tailwindcss: "tailwindcss",
+  shadcnui: "ShadCN UI",
+  styledcomponents: "Styled-components",
+  tailwindcss: "Tailwind CSS",
   tanstackrouter: "TanStack Router",
 } as const;
 
-export const skills = {
-  communicate: "Communicate",
-  eagleToLearn: "Eager to Learn",
-  problemSolving: "Problem Solving",
-  projectManagement: "Project Management",
-  timeManagement: "Time Management",
-} as const;
+export const experiences = [
+  {
+    date: "Apr 2024 - current",
+    title: "Executive Frontend Developer",
+    company: "HiSEVEN Sdn Bhd",
+    tasks: [
+      "Engineered a headless CMS architecture using Next.js with Tailwind CSS for the frontend, leveraging GraphQL and RESTful APIs.",
+      "Extended Ant Design to build a custom UI component library, ensuring design consistency.",
+      "Implemented a monorepo development structure using Nx for efficient code sharing.",
+      "Designed and developed a responsive, multilingual company website.",
+    ],
+  },
+  {
+    date: "Jul 2023 - Oct 2023",
+    title: "Frontend Developer",
+    company: "Avexis Solution Sdn Bhd",
+    tasks: [
+      "Migrated and improved a cryptocurrency system using Angular.",
+      "Created reusable components using a divide-and-conquer approach.",
+      "Prototyped mobile UI designs in Figma.",
+      "Developed a mobile tracking app with Ionic Framework.",
+    ],
+  },
+  {
+    date: "Mar 2021 - Jun 2021",
+    title: "Frontend Developer",
+    company: "Integrated Carrier Express Sdn Bhd",
+    tasks: [
+      "Streamlined parcel tracking with VBA scripts.",
+      "Created logistics interface mockups using Adobe XD.",
+      "Liaised with technical partners for delivery integrations.",
+      "Maintained web systems and Shopee storefronts.",
+    ],
+  },
+];
