6ef2124ba443c4362d9fcdbdb7764b785da7ddae
fattybun <fattybun1128@gmail.com>
2025-03-20 15:49:31 +0800

    Add portfolio

--- src/configs/portfolio.ts
+++ src/configs/portfolio.ts
@@ -6,4 +6,11 @@ export const portfoliosURL = {
     excerpt:
       "This system is an event and task manager that offers both a calendar overview and a detailed list view. It enables users to create, view, and manage upcoming commitments, helping them stay organized and on schedule.",
   },
+  todo_app: {
+    url: "https://todo-next-ten-ebon.vercel.app/",
+    title: "Simple ToDo",
+    preview: "/portfolio-image/simple-to-do.png",
+    excerpt:
+      "This to-do app helps users add, view, edit, and delete tasks through a simple interface, making task management straightforward and efficient.",
+  },
 };
